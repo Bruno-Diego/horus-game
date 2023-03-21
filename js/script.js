01f3bff20531186f2a0e7f394d07dc84afb4b530
BrunoDS <bruno.diego.x@hotmail.com>
2023-03-21 12:25:28 +0000

    Add gaming functionality

--- js/script.js
+++ js/script.js
@@ -13,29 +13,29 @@ function Progress() {
     this.updateScore(0)
 }
 
-function Bird(heightGame) {
-    let voando = false
+function Bird(gameHeight) {
+    let flying = false
     this.element = newElement('img', 'bird')
     this.element.src = 'img/horus.png'
     this.getY = () => parseInt(this.element.style.bottom.split('px')[0])
     this.setY = y => this.element.style.bottom = `${y}px`
 
-    // window.onkeydown = e => voando = true
-    // window.onkeyup = e => voando = false
+    window.onkeydown = e => flying = true
+    window.onkeyup = e => flying = false
 
-    // this.animar = () => {
-    //     const novoY = this.getY() + (voando ? 8 : -5)
-    //     const alturaMaxima = alturaJogo - this.element.clientHeight
+    this.animate = () => {
+        const newY = this.getY() + (flying ? 8 : -5)
+        const maxHeight = gameHeight - this.element.clientHeight
 
-    //     if (novoY <= 0) {
-    //         this.setY(0)
-    //     } else if (novoY >= alturaMaxima) {
-    //         this.setY(alturaMaxima)
-    //     } else {
-    //         this.setY(novoY)
-    //     }
-    // }
-    // this.setY(alturaJogo / 2)
+        if (newY <= 0) {
+            this.setY(0)
+        } else if (newY >= maxHeight) {
+            this.setY(maxHeight)
+        } else {
+            this.setY(newY)
+        }
+    }
+    this.setY(gameHeight / 2)
 }
 
 function Bar(reverse = false) {
@@ -75,6 +75,61 @@ function BarPair(height, spacing, x) {
     this.setX(x)
 }
 
+
+function Bars(height, width, opening, gap, notifyScore) {
+    this.pairs = [
+        new BarPair(height, opening, width),
+        new BarPair(height, opening, width + gap),
+        new BarPair(height, opening, width + gap * 2),
+        new BarPair(height, opening, width + gap * 3),
+    ]
+
+    const movement = 3;
+    this.animate = () => {
+        this.pairs.forEach(pair => {
+            pair.setX(pair.getX() - movement)
+
+            //quando o elemento sair da area do jogo
+            if (pair.getX() < -pair.getWidth()) {
+                pair.setX(pair.getX() + gap * this.pairs.length)
+                pair.getSpacing()
+            }
+
+            const between = width / 2
+            const crossBetween = pair.getX() + movement >= between
+                && pair.getX() < between;
+            if (crossBetween) notifyScore();
+        });
+    }
+}
+
+
+function isOver(elementA, elementB) {
+    const a = elementA.getBoundingClientRect()
+    const b = elementB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left 
+        && b.left + b.width >= a.left
+
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function colided(bird, bars) {
+    let colided = false
+    bars.pairs.forEach(barPairs => {
+        if(!colided) {
+            const superior = barPairs.superior.element
+            const inferior = barPairs.inferior.element
+            colided = isOver(bird.element, superior)
+                || isOver(bird.element, inferior)
+        }
+    })
+    return colided
+}
+
 function Horus() {
     let score = 0
     const gameArea = document.querySelector('[wm-flappy]')
@@ -82,10 +137,25 @@ function Horus() {
     const width = gameArea.clientWidth
     const bird = new Bird(height)
     const progress = new Progress()
+    // const b = new BarPair(height, 200, 400)
+    // document.querySelector('[wm-flappy]').appendChild(b.element)
+    const bars = new Bars(height, width, 200, 400,
+        () => progress.updateScore(++score))
+        
     gameArea.appendChild(bird.element)
     gameArea.appendChild(progress.element)
+    bars.pairs.forEach(pair => gameArea.appendChild(pair.element))
 
-    const b = new BarPair(500, 200, 400)
-    document.querySelector('[wm-flappy]').appendChild(b.element)
+    this.start = () => {
+        // loop do jogo
+        const timer = setInterval(() => {
+            bars.animate()
+            bird.animate()
+
+            if(colided(bird, bars)) {
+                clearInterval(timer)
+            }
+        }, 20)
+    }
 }
-new Horus()
+new Horus().start()
